695383bbb4586cc030220d10f5a263afd8587234
276726581 <276726581@qq.com>
2018-07-23 14:17:53 +0800

    DiscoveryProxyPool

---
+++ src/main/java/com/tiho/http/gateway/client/DiscoveryProxyPool.java
@@ -0,0 +1,30 @@
+package com.tiho.http.gateway.client;
+
+import com.tiho.http.gateway.exception.NotFoundException;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+
+import java.net.InetSocketAddress;
+
+public class DiscoveryProxyPool implements ProxyPool {
+
+    private String gatewayServiceName;
+    private LoadBalancerClient loadBalancerClient;
+
+    public DiscoveryProxyPool(String gatewayServiceName, LoadBalancerClient loadBalancerClient) {
+        this.gatewayServiceName = gatewayServiceName;
+        this.loadBalancerClient = loadBalancerClient;
+    }
+
+    @Override
+    public InetSocketAddress getInetSocketAddress() {
+        ServiceInstance instance = loadBalancerClient.choose(gatewayServiceName);
+        if (null == instance) {
+            throw new NotFoundException();
+        }
+        String host = instance.getHost();
+        int port = instance.getPort();
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        return address;
+    }
+}
