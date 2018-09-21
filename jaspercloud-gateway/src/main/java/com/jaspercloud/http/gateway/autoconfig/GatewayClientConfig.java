bcbfb664251817a06538d71009da22312fb197b0
276726581 <276726581@qq.com>
2018-09-21 15:34:52 +0800

    从LoadBalancerClient获取GatewayServer地址

--- jaspercloud-gateway/src/main/java/com/jaspercloud/http/gateway/autoconfig/GatewayClientConfig.java
+++ jaspercloud-gateway/src/main/java/com/jaspercloud/http/gateway/autoconfig/GatewayClientConfig.java
@@ -6,11 +6,13 @@ import feign.Request;
 import feign.Retryer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDiscoveryClient
 public class GatewayClientConfig {
 
     @ConditionalOnMissingBean
